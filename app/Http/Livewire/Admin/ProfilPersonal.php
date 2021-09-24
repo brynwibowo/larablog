58acdbf55d87e9392727f38c6d0a8e35e3f428c9
brynwibowo <brynwibowo@gmail.com>
2021-09-24 15:27:00 +0700

    add component profil personal

---
+++ app/Http/Livewire/Admin/ProfilPersonal.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class ProfilPersonal extends Component
+{
+    public function render()
+    {
+        return <<<'blade'
+            <div>
+                {{-- coming soon! --}}
+            </div>
+        blade;
+    }
+}
